e6f047f57e2a7c8b4ad821f782c68d2ccdd3b0c4
Shengwen Cheng <shengwen1997.tw@gmail.com>
2016-08-31 12:37:58 +0800

    Fix CMake/Makefile absoluted path

--- ros2_ws/rostoolchain.cmake
+++ ros2_ws/rostoolchain.cmake
@@ -1,24 +1,32 @@
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_C_COMPILER  /usr/bin/arm-linux-gnueabi-gcc-5) 
-set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabi-g++-5)
+set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(BUILD_TESTING 0)
+#set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
-set(TARGET_ROOT /home/shengwen-asus/ros2_linux_root)
-set(CROSS_COMPILE_ROOT /usr/arm-linux-gnueabi/)
+set(BUILD_TESTING 0)
+add_definitions(-DCOMPILE_EXAMPLES=OFF)
+
+set(CMAKE_VERBOSE_MAKEGILE ON)
+
+set(TARGET_ROOT /home/shengwen-asus/cross-compile-ros2/rootfs)
+set(CROSS_COMPILE_ROOT /home/shengwen-asus/cross-compile-ros2/cross_compiler/arm-unknown-linux-gnueabihf/arm-unknown-linux-gnueabihf/sysroot)
 
 set(
   CMAKE_FIND_ROOT_PATH
-  ${TARGET_ROOT}
   ${CROSS_COMPILE_ROOT}
+  ${TARGET_ROOT}
 )
 
-#set(BOOST_ROOT ${TARGET_ROOT}/usr/include/boost)
 set(BOOST_INCLUDEDIR ${TARGET_ROOT}/usr/include/)
 set(BOOST_LIBRARYDIR ${TARGET_ROOT}/usr/lib/arm-linux-gnueabihf/)
+
+#set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${TARGET_ROOT}/opt/ros/kinetic/share/roscpp)
+#set(roscpp_PREFIX ${TARGET_ROOT}/opt/ros/kinetic/share/roscpp)
+#set(roscpp_INCLUDEDIR ${TARGET_ROOT}/opt/ros/kinetic/include/roscpp)
+#set(roscpp_LIBDIR ${TARGET_ROOT}/opt/ros/kinetic/lib)
 
 #cmake --help-module FindPythonLibs | tail -10
 set(PYTHON_LIBRARY ${TARGET_ROOT}/usr/lib/python3.5/config-3.5m-arm-linux-gnueabihf/)
@@ -28,4 +36,4 @@ set(
   ${TARGET_ROOT}/usr/include/python3.5m/
 )
 
-include_directories(${TARGET_ROOT}/usr/include/arm-linux-gnueabihf/python3.5m/)
+include_directories(${TARGET_ROOT}/usr/include/python3.5m/)
