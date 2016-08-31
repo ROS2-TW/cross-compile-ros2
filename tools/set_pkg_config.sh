5df6b69f7e7ffb74f24e999ddc23d66bb61c74a8
Shengwen Cheng <shengwen1997.tw@gmail.com>
2016-08-31 13:00:18 +0800

    Arrange tool scripts path

--- tools/set_pkg_config.sh
+++ tools/set_pkg_config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ROOTFS=${PWD}/rootfs
+ROOTFS=${PWD}/../rootfs
 
 export PKG_CONFIG_DIR=${ROOTFS}/usr/lib/arm-linux-gnueabihf/pkgconfig:${ROOTFS}/opt/ros/kinetic/lib/pkgconfig
 export PKG_CONFIG_LIBDIR=${ROOTFS}/usr/lib/arm-linux-gnueabihf/pkgconfig:${ROOTFS}/opt/ros/kinetic/lib/pkgconfig
 export PKG_CONFIG_SYSROOT_DIR=${ROOTFS}
 
 #Test
-pkg-config --cflags glib-2.0
-pkg-config --cflags --libs roscpp
+#pkg-config --cflags glib-2.0
+#pkg-config --cflags --libs roscpp
