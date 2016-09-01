a6bbabd2150a572e8f7eff5a5a36676543357924
Shengwen Cheng <shengwen1997.tw@gmail.com>
2016-09-01 19:00:51 +0800

    Fix path

--- tools/set_rootfs.sh
+++ tools/set_rootfs.sh
@@ -4,7 +4,7 @@ USER=ubuntu
 HOST=raspi #e.g: 192.168.X.X
 
 #Step.1: Copy files
-rsync -rl --delete-after --info=progress2  ubuntu@raspi:/{lib,usr,opt,etc} $PWD/rootfs/
+rsync -rl --delete-after --info=progress2  ubuntu@raspi:/{lib,usr,opt,etc} ../rootfs/
 
 #Step.2: Replace rootfs abosulted path to related path
-tools/fixQualifiedLibraryPaths rootfs cross_compiler/arm-unknown-linux-gnueabihf/bin/arm-unknown-linux-gnueabihf-gcc
+./fixQualifiedLibraryPaths ../rootfs ../cross_compiler/arm-rpi-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc
