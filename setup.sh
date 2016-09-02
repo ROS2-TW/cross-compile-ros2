d9316359825e358fce5cc128d3b27c481c51b6e9
Shengwen Cheng <shengwen1997.tw@gmail.com>
2016-09-02 22:10:02 +0800

    Add environment setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Download Linaro gnu toolchain 5.31
+wget -nc  https://releases.linaro.org/components/toolchain/binaries/5.3-2016.05/arm-linux-gnueabihf/gcc-linaro-5.3.1-2016.05-x86_64_arm-linux-gnueabihf.tar.xz
+
+#Compress the toolchain
+tar xvf gcc-linaro-5.3.1-2016.05-x86_64_arm-linux-gnueabihf.tar.xz
+
