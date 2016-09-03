9043bbb170464f950d9311bdc264c8a0cc48f239
Shengwen Cheng <shengwen1997.tw@gmail.com>
2016-09-04 01:40:58 +0800

    Add ros1 environment setup script

---
+++ tools/set_ros1_env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ROOTFS=$(realpath ../rootfs)
+
+export CMAKE_PREFIX_PATH="$ROOTFS/opt/ros/kinetic"
+export PKG_CONFIG_PATH="$ROOTFS/opt/ros/kinetic/lib/pkgconfig"
+
+export ROSLISP_PACKAGE_DIRECTORIES=""
+export ROS_DISTRO="kinetic"
+export ROS_ETC_DIR="$ROOTFS/opt/ros/kinetic/etc/ros"
+export ROS_MASTER_URI="http://localhost:11311"
+export ROS_PACKAGE_PATH="$ROOTFS/opt/ros/kinetic/share:$ROOTFS/opt/ros/kinetic/stacks"
+export ROS_ROOT="$ROOTFS/opt/ros/kinetic/share/ros"
