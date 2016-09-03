26bcca5c96d59344d07fe879c7bb830ef49ef5ca
Shengwen Cheng <shengwen1997.tw@gmail.com>
2016-09-03 15:28:17 +0800

    Fix more path

--- tools/fix_ros1_so.sh
+++ tools/fix_ros1_so.sh
@@ -2,10 +2,26 @@
 
 ROOTFS=$(realpath ../rootfs)
 
-cd ../rootfs/opt/ros/kinetic/share
+PWD_SAVE=$PWD
+
+cd ${PWD_SAVE}/../rootfs/opt/ros/kinetic/share
 
 #Replace "/usr/lib/arm-linux/gnueabihf" with "$ROOTFS/usr/lib/arm-linux/gnueabihf"
 sudo grep -rl "/usr/lib/arm-linux-gnueabihf/" * | sudo xargs sed -i "s|/usr/lib/arm-linux-gnueabihf/|$ROOTFS/usr/lib/arm-linux-gnueabihf/|g"
 
 #Replace "/opt" with "$ROOTFS/opt"
 sudo grep -rl "/opt/" * | sudo xargs sed -i "s|/opt/|$ROOTFS/opt/|g"
+
+#Replace "/usr/include" with "$ROOTFS/usr/include"
+sudo grep -rl "/usr/include" | sudo xargs sed -i "s|/usr/include|$ROOTFS/usr/include|g"
+
+cd ${PWD_SAVE}/../rootfs/opt/ros/kinetic/lib/pkgconfig
+
+#Replace "/usr/lib/arm-linux/gnueabihf" with "$ROOTFS/usr/lib/arm-linux/gnueabihf"
+sudo grep -rl "\-l:/usr/lib/arm-linux-gnueabihf/" * | sudo xargs sed -i "s|/usr/lib/arm-linux-gnueabihf/|$ROOTFS/usr/lib/arm-linux-gnueabihf/|g"
+
+#Replace "/opt" with "$ROOTFS/opt"
+sudo grep -rl "/opt/" * | sudo xargs sed -i "s|/opt/|$ROOTFS/opt/|g"
+
+#Replace "/usr/include" with "$ROOTFS/usr/include"
+sudo grep -rl "/usr/include" * | sudo xargs sed -i "s|/usr/include|$ROOTFS/usr/include|g"
