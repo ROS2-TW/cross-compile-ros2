ae9f86a85667e14e4e481f7c051ae4db75374183
Shengwen Cheng <shengwen1997.tw@gmail.com>
2016-09-10 12:31:03 +0800

    Replace username and hostname with variables

--- rsync_target.sh
+++ rsync_target.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+USER=ubuntu
+HOSTNAME=raspi
+
 #Copy ros2_ws
-rsync -rl --delete-after --info=progress2 ./rootfs/workspace/ros2_ws ubuntu@raspi:~
+rsync -rl --delete-after --info=progress2 ./rootfs/workspace/ros2_ws $USER@$HOSTNAME:~
 #Copy resolve_target_symlink.sh
-rsync -rl --delete-after --info=progress2 ./tools/resolve_target_symlink.sh ubuntu@raspi:~/ros2_ws/resolve_target_symlink.sh
+rsync -rl --delete-after --info=progress2 ./tools/resolve_target_symlink.sh $USER@$HOSTNAME:~/ros2_ws/resolve_target_symlink.sh
--- tools/set_rootfs.sh
+++ tools/set_rootfs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 USER=ubuntu
-HOST=raspi #e.g: 192.168.X.X
+HOSTNAME=raspi #e.g: 192.168.X.X
 
 #Step.1: Copy files
-rsync -rl --delete-after --info=progress2  ubuntu@raspi:/{lib,usr,opt,etc} ../rootfs/
+rsync -rl --delete-after --info=progress2  $USER@$HOSTNAME:/{lib,usr,opt,etc} ../rootfs/
 
 #Step.2: Replace rootfs abosulted path to related path
 ./fixQualifiedLibraryPaths ../rootfs ../cross_compiler/bin/arm-linux-gnueabihf-gcc
